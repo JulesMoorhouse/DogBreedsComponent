556e8db309085c63ba8ec7c3d6c185a1d037c415
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2022-12-29 16:55:16 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DogBreedsComponent
+
+A description of this package.
---
+++ Sources/DogBreedsComponent/DogBreedsComponent.swift
@@ -0,0 +1,6 @@
+public struct DogBreedsComponent {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
